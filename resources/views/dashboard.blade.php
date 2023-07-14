b347c8e682cb514d17e93f3f599e89479625ee89
Cristiancg <45461497+armindnwyr@users.noreply.github.com>
2023-07-14 11:09:26 -0500

    dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,11 +3,30 @@
 @section('title', 'Dashboard')
 
 @section('content_header')
-    <h1>HELPDESK</h1>
 @stop
 
 @section('content')
-    <p>Buen día</p>
+
+<div class="row mt-4">
+<div class="col-md-4">
+    <x-adminlte-small-box title="40" text="Dispositivos Registrados" icon="fas fa-users text-white"
+    theme="dark" url="#" url-text="Ver Detalles"/>
+</div>
+
+<div class="col-md-4">
+    <x-adminlte-small-box title="12" text="Incidencias Registrados" icon="fa fa-file-pdf text-white"
+    theme="info" url="#" url-text="Ver Detalles"/>
+</div>
+
+<div class="col-md-4">
+    <x-adminlte-small-box title="50" text="Solucionados Registrados" icon="fa fa-users text-white"
+    theme="dark" url="#" url-text="Ver Detalles" id="sbUpdatable"/>
+</div>
+
+<div class="col-md-4 offset-md-4">
+    <x-adminlte-small-box title="5" text="Suspendidos Registrados" icon="fas fa-book text-white"
+    theme="info" url="#" url-text="Ver Detalles" id="sbUpdatable"/>
+</div>
 @stop
 
 @section('css')
